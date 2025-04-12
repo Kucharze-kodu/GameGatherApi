442b4379b305f7cb3f43c8893696a501715b4416
Jakub Popiolek <grimurin@gmail.com>
2025-04-12 12:58:07 +0200

    Modify funtions to hash and verify password

--- src/GameGather.Infrastructure/Utils/PasswordHasher.cs
+++ src/GameGather.Infrastructure/Utils/PasswordHasher.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography;
+using System.Text;
 using GameGather.Application.Utils;
+using Konscious.Security.Cryptography;
 
 namespace GameGather.Infrastructure.Utils;
 
@@ -7,15 +9,16 @@ public sealed class PasswordHasher : IPasswordHasher
 {
     private const int SaltSize = 16;
     private const int KeySize = 32;
-    private const int Iterations = 10000;
+    private const int DegreeOfParallelism = 8;
+    private const int MemorySize = 1024 * 1024;
+    private const int Iterations = 4;
 
     private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA256;
     
     public string Hash(string password)
     {
         byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
-        byte[] hash = Rfc2898DeriveBytes
-            .Pbkdf2(password, salt, Iterations, Algorithm, KeySize);
+        byte[] hash = HashPassword(password, salt);
         
         return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
     }
@@ -26,9 +29,21 @@ public sealed class PasswordHasher : IPasswordHasher
         var hash = Convert.FromHexString(hashParts[0]);
         var salt = Convert.FromHexString(hashParts[1]);
         
-        var inputHash = Rfc2898DeriveBytes
-            .Pbkdf2(password, salt, Iterations, Algorithm, KeySize);
+        var inputHash = HashPassword(password, salt);
         
-        return hash.SequenceEqual(inputHash);
+        return CryptographicOperations.FixedTimeEquals(inputHash, hash);
+    }
+    
+    private byte[] HashPassword(string password, byte[] salt)
+    {
+        var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+        {
+            Salt = salt,
+            DegreeOfParallelism = DegreeOfParallelism,
+            Iterations = Iterations,
+            MemorySize = MemorySize
+        };
+
+        return argon2.GetBytes(KeySize);
     }
 }
