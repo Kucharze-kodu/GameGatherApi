b9184955bde203a0b51ac2c45947d79f0626a593
Bartek-Pisklak <bartek.pisklak@onet.pl>
2025-05-29 20:09:38 +0200

    repair branch
    delete file, and repair migrations

--- src/GameGather.Application/Features/SessionGames/Commands/DeleteSessionGames/DeleteSessionGameCommnand.cs
+++
@@ -1,9 +0,0 @@
-﻿using GameGather.Application.Common.Messaging;
-using GameGather.Application.Contracts.SessionGames;
-
-namespace GameGather.Application.Features.SessionGames.Commands.DeleteSessionGames
-{
-    public record DeleteSessionGameCommnand(
-        int Id
-        ) : ICommand<SessionGameResponse>;
-}
