02bc939b9c113b10435542f213024f3b9e72f762
Jakub Popiolek <grimurin@gmail.com>
2025-06-04 21:49:15 +0200

    Add unit tests for register user command validator

--- GameGather.UnitTests.Utils/Builders/RegisterUser/RegisterUserCommandBuilder.cs
+++ GameGather.UnitTests.Utils/Builders/RegisterUser/RegisterUserCommandBuilder.cs
@@ -12,7 +12,53 @@ public class RegisterUserCommandBuilder
     private DateTime _birthday = Constants.User.Birthday;
     private string _verifyEmailUrl = Constants.User.VerifyEmailUrl;
 
+    public RegisterUserCommandBuilder WithFirstName(string firstName)
+    {
+        _firstName = firstName;
+        return this;
+    }
     
+    public RegisterUserCommandBuilder WithFirstNameMinimum100CharactersLong()
+    {
+        _firstName = new string('A', 101);
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithLastName(string lastName)
+    {
+        _lastName = lastName;
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithLastNameMinimum100CharactersLong()
+    {
+        _lastName = new string('A', 101);
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithEmail(string email)
+    {
+        _email = email;
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithPassword(string password)
+    {
+        _password = password;
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithConfirmPassword(string confirmPassword)
+    {
+        _confirmPassword = confirmPassword;
+        return this;
+    }
+    
+    public RegisterUserCommandBuilder WithBirthday(DateTime birthday)
+    {
+        _birthday = birthday;
+        return this;
+    }
 
     public RegisterUserCommand Build()
     {
---
+++ test/UnitTests/GameGather.Application.UnitTests/Features/Users/Commands/RegisterUser/RegisterUserCommandValidatorTests.cs
@@ -0,0 +1,240 @@
+using FluentValidation.TestHelper;
+using GameGather.Application.Features.Users.Commands.RegisterUser;
+using GameGather.UnitTests.Utils.Builders.RegisterUser;
+
+namespace GameGather.Application.UnitTests.Features.Users.Commands.RegisterUser;
+
+public class RegisterUserCommandValidatorTests
+{
+    private readonly RegisterUserCommandValidator _registerUserCommandValidator = new();
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task FirstName_Should_NotBeEmpty(string firstName)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithFirstName(firstName)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.FirstName);
+    }
+    
+    [Fact]
+    public async Task FirstName_Should_BeMaximum100CharactersLong()
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithFirstNameMinimum100CharactersLong()
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.FirstName);
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task LastName_Should_NotBeEmpty(string lastName)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithLastName(lastName)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.LastName);
+    }
+
+    [Fact]
+    public async Task LastName_Should_BeMaximum100CharactersLong()
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithLastNameMinimum100CharactersLong()
+            .Build();
+        
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+        
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.LastName);
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task Email_Should_NotBeEmpty(string email)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithEmail(email)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.Email);
+    }
+    
+    [Theory]
+    [InlineData("invalid-email")]
+    [InlineData("invalid-email@")]
+    [InlineData("@domain")]
+    public async Task Email_Should_BeValid(string email)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithEmail(email)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.Email);
+    }
+    
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task Password_Should_NotBeEmpty(string password)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithPassword(password)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.Password);
+    }
+    
+    [Theory]
+    [InlineData("a")]
+    [InlineData("a2")]
+    [InlineData("a2c")]
+    [InlineData("a2c3")]
+    [InlineData("a2c3d4")]
+    [InlineData("1234567")]
+    public async Task Password_Should_BeAtLeast8CharactersLong(string password)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithPassword(password)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.Password);
+    }
+    
+    [Theory]
+    [InlineData("test")]
+    [InlineData("P4ssw0rd!")]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public async Task ConfirmPassword_Should_MatchPassword(string confirmPassword)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithPassword("OtherPassword")
+            .WithConfirmPassword(confirmPassword)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.ConfirmPassword);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    public async Task Birthday_Should_NotBeEmpty(DateTime birthday)
+    {
+        // Arrange
+        
+        var command = new RegisterUserCommandBuilder()
+            .WithBirthday(birthday)
+            .Build();
+
+        // Act
+        
+        var result = await _registerUserCommandValidator
+            .TestValidateAsync(command);
+        
+        // Assert
+        
+        result
+            .ShouldHaveValidationErrorFor(x => x.Birthday);
+            
+    }
+    
+}
