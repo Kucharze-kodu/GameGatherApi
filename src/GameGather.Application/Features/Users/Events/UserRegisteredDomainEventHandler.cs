199e8f368bca891115e1204e1d1ae8f4ed7cd6d1
Jakub Popiolek <grimurin@gmail.com>
2025-06-05 21:09:54 +0200

    Add unit tests for user registered domain event handler

--- src/GameGather.Application/Features/Users/Events/UserRegisteredDomainEventHandler.cs
+++ src/GameGather.Application/Features/Users/Events/UserRegisteredDomainEventHandler.cs
@@ -24,19 +24,19 @@ public sealed class UserRegisteredDomainEventHandler : INotificationHandler<User
 
     public async Task Handle(UserRegistered notification, CancellationToken cancellationToken)
     {
-        await _emailService.SendEmailWithVerificationTokenAsync(
-            notification.Email,
-            notification.FirstName,
-            notification.VerificationToken.ToString(),
-            notification.VerifyEmailUrl,
-            cancellationToken);
-        
         var user = await _userRepository.GetByEmailAsync(notification.Email, cancellationToken);
         
         if (user is null)
         {
             throw new InvalidOperationException("User not found");
         }
+        
+        await _emailService.SendEmailWithVerificationTokenAsync(
+            notification.Email,
+            notification.FirstName,
+            notification.VerificationToken.ToString(),
+            notification.VerifyEmailUrl,
+            cancellationToken);
         
         user.VerificationToken.UpdateLastSendOnUtc();
         await _unitOfWork.SaveChangesAsync(cancellationToken);
