0c510c6051918e34075364e5e26761eb6b9e3040
Jakub Popiołek <grimurin@gmail.com>
2025-06-15 15:40:43 +0200

    Add functional tests to login user functionality
    - Add base functional test
    - Add functional test web app factory
    - Modify directory structure of tests

---
+++ test/FunctionalTests/GameGather.FunctionalTests/Abstractions/FunctionalTestWebAppFactory.cs
@@ -0,0 +1,51 @@
+﻿using GameGather.Infrastructure.Persistance;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+
+namespace GameGather.FunctionalTests.Abstractions;
+
+public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("gamegather")
+        .WithUsername("postgres")
+        .WithPassword("password")
+        .WithPortBinding(62667, 5432)
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor = services
+                .SingleOrDefault(r => r.ServiceType == typeof(DbContextOptions<GameGatherDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<GameGatherDbContext>(options =>
+            {
+                var connectionString = _dbContainer.GetConnectionString();
+                options.UseNpgsql(connectionString);
+            });
+
+        });
+    }
+    
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+}
