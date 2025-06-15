4e89b9a1043660172f73ab68e24daf77e3029878
Jakub Popiołek <grimurin@gmail.com>
2025-06-15 15:40:43 +0200

    Add functional tests to login user functionality
    - Add base functional test
    - Add functional test web app factory
    - Modify directory structure of tests

---
+++ test/FunctionalTests/GameGather.FunctionalTests/Abstractions/BaseFunctionalTest.cs
@@ -0,0 +1,47 @@
+﻿using GameGather.Application.Utils;
+using GameGather.Infrastructure.Persistance;
+using GameGather.IntegrationTests.TestUtils.Seeders;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace GameGather.FunctionalTests.Abstractions;
+
+public abstract class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>, IAsyncLifetime
+{
+    private readonly IServiceScope _scope;
+    protected readonly HttpClient HttpClient; 
+    protected readonly ISender Sender; 
+    protected readonly GameGatherDbContext DbContext;
+    protected readonly TestDataSeeder DataSeeder;
+    protected readonly IPasswordHasher PasswordHasher;
+    
+    protected BaseFunctionalTest(FunctionalTestWebAppFactory factory)
+    {
+        _scope = factory
+            .Services
+            .CreateScope();
+        HttpClient = factory.CreateClient();
+        Sender = _scope
+            .ServiceProvider
+            .GetRequiredService<ISender>();
+        DbContext = _scope
+            .ServiceProvider
+            .GetRequiredService<GameGatherDbContext>();
+        PasswordHasher = _scope
+            .ServiceProvider
+            .GetRequiredService<IPasswordHasher>();
+        
+        DataSeeder = new TestDataSeeder(DbContext, PasswordHasher);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await DataSeeder.SeedAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
